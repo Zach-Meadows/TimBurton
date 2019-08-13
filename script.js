ebc862dcb0cae250491e7dab081fc8e865c9c31c
Zach Meadows <zmeadows1@gmail.com>
2019-08-12 21:11:56 -0400

    added html

--- script.js
+++ script.js
@@ -4,10 +4,41 @@ function christmasTown() {
 	let color = "everywhere";
 	whats = this;
 	let theAir = [paper, polar_bear, milk, snowflake];
+	whats = this;
 	ican !== myEyes;
 	i = dreaming;
 	jack("wake up");
 	this !== fair;
+	whats = this;
+	whats = this;
+	there = somethingVeryWrong;
+	whats = this;
+	const people = {
+		singing: true,
+		style: "songs"
+	};
+	whats = this;
+	const streets = {
+		litteCreatures: {
+			expression: "laughing"
+		}
+	}
+	let everyone = "happy";
+	I == daffy;
 	what = this;
-
+	whats = this;
+	const children = {
+		throwing: snowballs && !== heads
+	}
+	const they = {
+		status: "busy",
+		building: "toys"
+	} && {nobody: dead}
+	const house{
+		window: frosted
+	}
+	iBelieve !== myEyes;
+	const myBones{
+		feel: document.getElementById('inside').innerHTML
+	}
 }
