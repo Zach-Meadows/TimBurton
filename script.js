33907f1784850ee787a090410dfa2554b51edac1
Zach Meadows <zmeadows1@gmail.com>
2019-08-12 20:30:30 -0400

    first verse

---
+++ script.js
@@ -0,0 +1,13 @@
+function christmasTown() {
+	let whats = this;
+	whats = this;
+	let color = "everywhere";
+	whats = this;
+	let theAir = [paper, polar_bear, milk, snowflake];
+	ican !== myEyes;
+	i = dreaming;
+	jack("wake up");
+	this !== fair;
+	what = this;
+
+}
